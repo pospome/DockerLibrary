64108e84868a3b11dc8ad8938a95ce7fe972d118
fumiyuki.takahashi <fumiyuki.takahashi@dena.com>
2016-05-05 21:13:02 +0900

    fix

--- docker-entrypoint-initdb.d/set_replication.sh
+++ docker-entrypoint-initdb.d/set_replication.sh
@@ -3,10 +3,10 @@
 sleep 10s
 
 # to master
-mysqldump -u root -h $MYSQL_MASTER_PORT_3306_TCP_ADDR -p$MYSQL_MASTER_ENV_MYSQL_ROOT_PASSWORD --all-databases --single-transaction --triggers --routines --events > /tmp/master_dump.sql
+mysqldump -u root -h mysql-master -p$MYSQL_MASTER_ENV_MYSQL_ROOT_PASSWORD --all-databases --single-transaction --triggers --routines --events > /tmp/master_dump.sql
 
 # to master
-mysql -u root -h $MYSQL_MASTER_PORT_3306_TCP_ADDR -p$MYSQL_MASTER_ENV_MYSQL_ROOT_PASSWORD -e "GRANT REPLICATION SLAVE ON *.* TO repl@'%' IDENTIFIED BY '$REPLICATION_USER_PASSWORD';"
+mysql -u root -h mysql-master -p$MYSQL_MASTER_ENV_MYSQL_ROOT_PASSWORD -e "GRANT REPLICATION SLAVE ON *.* TO repl@'%' IDENTIFIED BY '$REPLICATION_USER_PASSWORD';"
 
 # to slave
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "reset master;"
@@ -15,7 +15,7 @@ mysql -u root -p$MYSQL_ROOT_PASSWORD -e "reset master;"
 mysql -u root -p$MYSQL_ROOT_PASSWORD < /tmp/master_dump.sql
 
 # to slave
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CHANGE MASTER TO MASTER_HOST='$MYSQL_MASTER_PORT_3306_TCP_ADDR', MASTER_USER='repl', MASTER_PASSWORD='$REPLICATION_USER_PASSWORD', MASTER_AUTO_POSITION=1, MASTER_PORT=3306;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CHANGE MASTER TO MASTER_HOST='mysql-master', MASTER_USER='repl', MASTER_PASSWORD='$REPLICATION_USER_PASSWORD', MASTER_AUTO_POSITION=1, MASTER_PORT=3306;"
 
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "start slave;"
 
